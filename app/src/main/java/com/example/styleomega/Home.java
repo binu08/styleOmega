ebeb8f11476e68ca396d4e2f039df937db93087c
Binuka Sanjula <binuka.amarasighe@yahoo.com>
2020-01-20 11:25:39 +0530

    Linking homePage and logout

--- app/src/main/java/com/example/styleomega/Home.java
+++ app/src/main/java/com/example/styleomega/Home.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -33,6 +34,7 @@ public class Home extends AppCompatActivity {
     private void logout() {
         firebaseAuth.signOut();
         startActivity(new Intent(Home.this,MainActivity.class));
+        Toast.makeText(Home.this,"LogOut Successful",Toast.LENGTH_LONG).show();
         finish();
     }
 }
--- app/src/main/java/com/example/styleomega/MainActivity.java
+++ app/src/main/java/com/example/styleomega/MainActivity.java
@@ -104,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
                     Toast.makeText(MainActivity.this, "Login Successful.",Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                    Intent intent = new Intent(MainActivity.this,Home.class);
                     startActivity(intent);
                 }
                 else
