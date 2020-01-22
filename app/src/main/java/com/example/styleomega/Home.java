4cc748305bcd7253f89de4261cd60430c57e5775
Binuka Sanjula <binuka.amarasighe@yahoo.com>
2020-01-23 03:55:25 +0530

    Home Activity with Navigation Pane

--- app/src/main/java/com/example/styleomega/Home.java
+++ app/src/main/java/com/example/styleomega/Home.java
@@ -1,39 +1,124 @@
 package com.example.styleomega;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
 
 import io.paperdb.Paper;
 
-public class Home extends AppCompatActivity {
-    Button logoutbtn;
+public class Home extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        logoutbtn=findViewById(R.id.button_Home_logout);
-        logoutbtn.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 logout();
-             }
-         });
+        Paper.init(this);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Home");
+        setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_cart, R.id.nav_orders, R.id.nav_categories,
+                R.id.nav_settings, R.id.nav_logout, R.id.nav_gallery)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
-    private void logout() {
-        Paper.book().destroy(); //to logout
-        startActivity(new Intent(Home.this,MainActivity.class));
-        Toast.makeText(Home.this,"LogOut Successful",Toast.LENGTH_SHORT).show();
-        finish();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_cart) {
+//            Intent intent = new Intent(HomeActivity.this, CartActivity.class);
+//            startActivity(intent);
+
+//        } else if (id == R.id.nav_search) {
+
+//            Intent intent = new Intent(HomeActivity.this, SearchProductActivity.class);
+//            startActivity(intent);
+
+        } else if (id == R.id.nav_categories) {
+
+//            Intent intent = new Intent(HomeActivity.this, CategoryActivity.class);
+//            startActivity(intent);
+
+        } else if (id == R.id.nav_settings) {
+
+//            Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
+//            startActivity(intent);
+
+//        } else if (id == R.id.contact_us) {
+
+//            Intent intent = new Intent(HomeActivity.this, ContactUsActivity.class);
+//            startActivity(intent);
+
+        } else if (id == R.id.nav_logout) {
+
+            Paper.book().destroy();
+            Intent intent = new Intent(Home.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+
+        }
+//        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
